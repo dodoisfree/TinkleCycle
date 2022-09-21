e96d343b6bbc52d634ac3507fc5d3f498a9154dd
gyeongjae cheon <yocasd2@gmail.com>
2022-09-21 23:50:49 +0900

    .

--- client/src/pages/Community/Community.jsx
+++ client/src/pages/Community/Community.jsx
@@ -9,29 +9,61 @@ import MenuBar from "../../components/MenuBar";
 import styled from "styled-components";
 import BoardList from "../../components/Community/BoardList";
 import { Link } from "react-router-dom";
-import writeImg from "../../assets/img/edit.png";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 
+import Search1 from "../../assets/img/search-w.png";
+import writeImg from "../../assets/img/edit.png";
+
 const CommunityContainer = styled.div`
-    position: relative;
-    .addBoardIcon {
-        position: absolute;
-        width: 580px;
-        height: 30px;
+    .searchAddress {
+        margin: 40px 0;
+        width: 100%;
+        height: 40px;
+        background-color: #98d6f6;
+        color: white;
+        border-radius: 15px;
         display: flex;
-        justify-content: flex-end;
-
-        @media (max-width: 640px) {
-            width: 97%;
+        align-items: center;
+        > label {
+            width: 20%;
+            height: 100%;
+            border-right: 1px solid white;
+            h4 {
+                line-height: 40px;
+                text-align: center;
+                font-size: 16px;
+                font-weight: bold;
+            }
         }
-
-        .fixIcon {
-            position: fixed;
-            width: 30px;
-            img {
-                width: 35px;
-                height: 35px;
+        .addrIpt {
+            border: none;
+            background-color: #98d6f6;
+            color: white;
+            width: 70%;
+            height: 100%;
+            padding: 0;
+            padding-left: 30px;
+            box-sizing: border-box;
+            outline: none;
+            font-size: 18px;
+            border-right: 1px solid white;
+            &::placeholder {
+                color: white;
+            }
+        }
+        .searchBtn {
+            border: none;
+            width: 15%;
+            height: 100%;
+            background: url(${Search1}) no-repeat;
+            background-size: 30px, 30px;
+            background-position: center, center;
+            text-indent: -99999px;
+            &:hover {
+                background-color: #228ae6;
+                border-top-right-radius: 15px;
+                border-bottom-right-radius: 15px;
             }
         }
     }
@@ -65,12 +97,12 @@ const Community = memo(() => {
             <div className="containerSize media">
                 <form className="searchAddress" onSubmit={formik.handleSubmit}>
                     <label htmlFor="addrIpt">
-                        <h4>내주소</h4>
+                        <h4>새 글 작성</h4>
                     </label>
-                    <input id="addrIpt" className="addrIpt" type="address" name="keyword" placeholder="키워드 입력." value={formik.values.keyword} {...formik.getFieldProps("keyword")} />
+                    <input id="addrIpt" className="addrIpt" type="address" name="keyword" placeholder="게시글 검색." value={formik.values.keyword} {...formik.getFieldProps("keyword")} />
                     <button type="submit" className="searchBtn"> 검색 버튼 </button>
                 </form>
-                <Link to="/addBoard" className="fixIcon">
+                <Link to="/addBoard" className="addrIpt">
                     <img className="iconImg" src={writeImg} alt="writImg" />
                 </Link>
                 <BoardList />
