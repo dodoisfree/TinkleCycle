35547f116ab71119b2d757081ee36d3903595cf1
gyeongjae cheon <yocasd2@gmail.com>
2022-09-28 21:47:40 +0900

    .

--- client/src/pages/Attraction.jsx
+++ client/src/pages/Attraction.jsx
@@ -9,15 +9,10 @@ import {Routes, Route} from 'react-router-dom';
 import MenuBar from '../components/MenuBar';
 import AttractionContent from '../components/Attraction/AttractionContent';
 import AttractionMenu from '../components/Attraction/AttractionMenu';
-import styled from 'styled-components';
-
-const ATRT = styled.div`
-    background-color: aliceblue;
-`;
 
 const Attraction = memo(() => {
     return (
-        <ATRT>
+        <>
             <MenuBar />
             <AttractionMenu />
             <div className='containerSize media'>
@@ -25,7 +20,7 @@ const Attraction = memo(() => {
                     <Route path="/:api" element={<AttractionContent />} />
                 </Routes>
             </div>
-        </ATRT>
+        </>
     );
 });
 
