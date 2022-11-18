57fc8c609233594e682d9028b99cd1b04ae382e2
gyeongjae cheon <yocasd2@gmail.com>
2022-11-18 23:34:56 +0900

    .

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,65 +1,79 @@
-import React, { memo } from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-import logo from '../assets/img/logo.png'
+import React, { memo } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import logo from "../assets/img/logo.png";
 
 const HeaderContainer = styled.div`
-  height: 100px;
-  background-color: ${props => props.theme.color.BG};
+    height: 100px;
+    background-color: ${(props) => props.theme.color.BG};
 `;
 
 const InnerContainer = styled.div`
-  display: flex;
-  justify-content: left;
-  flex-direction: row;
+    display: flex;
+    justify-content: left;
+    flex-direction: row;
 `;
 
 const LiftContainer = styled.div`
-  width: 50%;
-  height: 100px;
-  .textBlind {
-    display: block;
-    width: 100px;
+    width: 50%;
     height: 100px;
-    background: url(${logo}) no-repeat center;
-    background-size: cover;
-  }
+    .textBlind {
+        display: block;
+        width: 100px;
+        height: 100px;
+        background: url(${logo}) no-repeat center;
+        background-size: cover;
+    }
 `;
 
 const RightContainer = styled.div`
-  width: 50%;
-  display: flex;
-  justify-content: right;
-  align-items: center;
+    width: 50%;
+    display: flex;
+    justify-content: right;
+    align-items: center;
     .login,
-    .join{
-      padding: 5px;
-      margin-right:10px;
-      border: 1px solid white;
-      color: white;
-      background-color: #98D6F6;
-      &:hover{
-        background-color: #228AE6;
-        border: 1px solid #228AE6;
-      }
+    .join {
+        padding: 5px;
+        margin-right: 10px;
+        border: 1px solid white;
+        color: white;
+        background-color: #98d6f6;
+        &:hover {
+            background-color: #228ae6;
+            border: 1px solid #228ae6;
+        }
     }
 `;
 const Header = memo(() => {
-  return (
-    <HeaderContainer className='containerSize media'>
-      <InnerContainer>
-        <LiftContainer>
-          <h1>
-            <Link to='/' className='textBlind'>우리동네 자전거 따르릉</Link>
-          </h1>
-        </LiftContainer>
-        <RightContainer>
-          <Link to='/login' className='login'>로그인</Link>
-          <Link to='/join' className='join'>회원가입</Link>
-        </RightContainer>
-      </InnerContainer>
-    </HeaderContainer>
-  );
+    const menuClick = React.useCallback((e) => {
+        const link = e.target;
+        link.style.border = '1px solid #228ae6';
+        link.style.backgroundColor = "#228ae6";
+        link.style.color = "white";
+
+    }, []);
+
+    const menuBlur = React.useCallback((e) => {
+        const link = e.target;
+        link.style.border = '1px solid white';
+        link.style.backgroundColor = "#98d6f6";
+        link.style.color = "white";
+    }, []);
+    return (
+        <HeaderContainer className="containerSize media">
+            <InnerContainer>
+                <LiftContainer>
+                    <h1>
+                        <Link to="/" className="textBlind">우리동네 자전거 따르릉</Link>
+                    </h1>
+                </LiftContainer>
+                <RightContainer>
+                    <Link to="/login" className="login" onClick={menuClick} onBlur={menuBlur}>로그인</Link>
+                    <Link to="/join" className="join" onClick={menuClick} onBlur={menuBlur}>회원가입</Link>
+                </RightContainer>
+            </InnerContainer>
+        </HeaderContainer>
+    );
 });
 
 export default Header;
