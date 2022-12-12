2989dbff1783f4a2003652aff3aac1ed9fb2d8c6
gyeongjae cheon <yocasd2@gmail.com>
2022-12-12 23:08:24 +0900

    .

--- client/src/components/Community/Section/AddComment.jsx
+++ client/src/components/Community/Section/AddComment.jsx
@@ -12,9 +12,9 @@ import {postItem} from '../../../slices/CommentSlice';
 import { useFormik } from "formik";
 import * as Yup from "yup";
 
-import AddBtn from '../../../assets/img/next.png'
+// import regexHelper from '../../../libs/RegexHelper';
 
-import regexHelper from '../../../libs/RegexHelper';
+import AddBtn from '../../../assets/img/next.png'
 
 const CommentContainer = styled.div`
     height: 100%;
@@ -67,36 +67,36 @@ const AddComment = memo(() =>{
             .max(15, "댓글은 최대 15글자 까지 가능합니다."),
         }),
         onSubmit: (values) => {
-          
+            window.alert('댓글이 등록 되었습니다.');
         },
       });
 
-  /**<form>의 submit 버튼이 눌러졌을 때 호출될 이벤트 핸들러 */
-  const onSubmit = React.useCallback((e)=>{
-    e.preventDefault();
+//   /**<form>의 submit 버튼이 눌러졌을 때 호출될 이벤트 핸들러 */
+//   const onSubmit = React.useCallback((e)=>{
+//     e.preventDefault();
 
-    // 이벤트가 발생한 폼 객체
-    const current = e.target;
-    //입력값에 대한 유효성 검사
-    try{
-        regexHelper.value(current.comment, '댓글을 입력하세요.');
-        regexHelper.minLength(current.comment, 2, '댓글은 최소 2글자 이상 입력해야합니다.');
-        regexHelper.maxLength(current.comment, 20, '댓글은 최대 15글자 까지 가능합니다.');
+//     // 이벤트가 발생한 폼 객체
+//     const current = e.target;
+//     //입력값에 대한 유효성 검사
+//     try{
+//         regexHelper.value(current.comment, '댓글을 입력하세요.');
+//         regexHelper.minLength(current.comment, 2, '댓글은 최소 2글자 이상 입력해야합니다.');
+//         regexHelper.maxLength(current.comment, 20, '댓글은 최대 15글자 까지 가능합니다.');
     
-        window.alert('댓글이 등록 되었습니다.');
-      }catch(e){
-      window.alert("댓글을 등록 하지 못했습니다.");
-      e.field.focus();
-      return;
-    }
+//         window.alert('댓글이 등록 되었습니다.');
+//       }catch(e){
+//       window.alert("댓글을 등록 하지 못했습니다.");
+//       e.field.focus();
+//       return;
+//     }
 
-    // 리덕스(Ajax처리)를 통해 데이터 저장 요청 --> 처리가 완료된 후 목록 페이지로 강제 이동한다.
-    // 비동기 처리이기 때문에 리덕스의 함수를 dispatch한 다음에 그에 대한 후속 처리를 한다면 
-    // 리덕스 자체가 promise객체이기 때문에 then을 사용해야한다
-    dispatch(postItem({
-      comment:current.comment.value
-    }))
-  },[dispatch]);
+//     // 리덕스(Ajax처리)를 통해 데이터 저장 요청 --> 처리가 완료된 후 목록 페이지로 강제 이동한다.
+//     // 비동기 처리이기 때문에 리덕스의 함수를 dispatch한 다음에 그에 대한 후속 처리를 한다면 
+//     // 리덕스 자체가 promise객체이기 때문에 then을 사용해야한다
+//     dispatch(postItem({
+//       comment:current.comment.value
+//     }))
+//   },[dispatch]);
 
  
     return (
@@ -105,9 +105,9 @@ const AddComment = memo(() =>{
        {error?(
         <ErrorView error={error}/>
        ) :(
-      <form className='commentBox' onSubmit={onSubmit}>
+      <form className='commentBox' onSubmit={formik.handleSubmit}>
           <input className='commentInput' type="text" placeholder="댓글을 작성해주세요" name="comment" />
-          <button type="submit" className='addBtn'></button>
+          <button className='addBtn' type="submit"></button>
         </form>
          )
         }
